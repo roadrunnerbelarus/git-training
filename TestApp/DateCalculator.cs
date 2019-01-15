7590db0d4687771a4d43e417f18bfb35655530b6
Vitali Shemet <vshemet@sii.pl>
2019-01-15 14:32:54 +0100

    DateCalculator introduced

---
+++ TestApp/DateCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public static class DateCalculator
+    {
+        internal static DateTime AddFromToday(int i)
+        {
+            var today = DateTime.Now;
+            return today.AddDays(i);
+        }
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,8 +9,7 @@ namespace TestApp
             Console.WriteLine("Enter the number of days: ");
             var i = Int32.Parse(Console.ReadLine());
 
-            var today = DateTime.Now;
-            var futureDay = today.AddDays(i);
+            var futureDay = DateCalculator.AddFromToday(i);
 
             Console.WriteLine($"In {i} days from today there will be \n{futureDay.Day} {futureDay.ToString("MMM")} {futureDay.Year}");
             Console.ReadKey();
