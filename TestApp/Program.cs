ed84b86f2e7dc9ce246ce7e10013ba44dcddfc64
Vitali Shemet <vshemet@sii.pl>
2019-01-16 11:50:04 +0100

    cs files modified

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,8 +7,8 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the number of days: ");
-            var i = Int32.Parse(Console.ReadLine());
 
+            var i = Int32.Parse(Console.ReadLine());
             var futureDay = DateCalculator.AddFromToday(i);
 
             Console.WriteLine($"In {i} days from today there will be \n{futureDay.Day} {futureDay.ToString("MMM")} {futureDay.Year} {futureDay.DayOfWeek}");
